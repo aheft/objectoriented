ee2562426234cd172038654f21b68f48817e416d
AJ Heft <aheft9@yahoo.ca>
2020-09-24 16:44:35 -0400

    lab 2

--- Lab 2/Test.java
+++ Lab 2/Test.java
@@ -4,8 +4,8 @@ import java.io.*;
 public class Test{
     public static void main(String[] args){
         Point test=new Point(5, 10, 20);
-        System.println(test.x);
-        System.println(test.y);
-        System.println(test.z);
+        System.out.println(test.x);
+        System.out.println(test.y);
+        System.out.println(test.z);
     }
 }
